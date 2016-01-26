ae75599773d1f370a3384a818ac4da75ad2c250b
pdxgitit <charles.webster04@gmail.com>
2016-01-25 21:36:47 -0800

    updated main

--- BlackJack/BlackJackMain.py
+++ BlackJack/BlackJackMain.py
@@ -1,3 +1,17 @@
 import BlackJackModel
 import BlackJackView
 import BlackJackController
+
+
+def main():
+    gameobject = Game()
+    viewobject = gameobject.view
+    number_players = viewobject.number_of_players_prompt()
+    name_players = viewobject.names_of_players(number_players)
+    gameobject.get_players(name_players)
+    gameobject.spawn_dealer()
+
+
+
+if __name__ == '__main__':
+    main()
