c5bc23429360058c291f8807714c7ec36973d356
pdxgitit <charles.webster04@gmail.com>
2016-01-25 18:46:06 -0800

    updated discard cards function

--- BlackJack/BlackJackController.py
+++ BlackJack/BlackJackController.py
@@ -91,6 +91,10 @@ class Game():
         if dealer.current_hand_value > 16:
             return False
 
+    def check_deck_empty(self):
+        if len(self.deck.cards) < 101:
+            return True
+
     def check_end_round(self):
         if self.player_object_list[self.current_player].dealer:
             return True
@@ -100,7 +104,10 @@ class Game():
 
     def discard_cards(self):
         for player in self.player_object_list:
-            self.player.append(self.player_object_list.pop())
+            for card in player.current_hand:
+                self.deck.append(card)
+            player.current_hand = []
+
 
     def deal_cards(self):
         for player in self.player_object_list:
